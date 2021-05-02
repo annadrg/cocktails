ca4d940de2294319e893379a456eaaa67afac192
annadrg <annaderuig@gmail.com>
2021-05-02 15:18:54 +0200

    Make Navbar responsive and add search

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,42 +1,54 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import "./NavBar.scss";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { useState } from "react";
 
 export default function NavBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="navBar">
-      <div>
+    <Navbar className="nav pr-2" bg="white" expand="sm">
+      <Navbar.Brand>
         <h2>Cocktails</h2>
-      </div>
-      <div className="navBarLink">
-        <NavLink
-          className="inactive"
-          activeClassName="active"
-          exact={true}
-          to="/"
-        >
-          Categories
-        </NavLink>{" "}
-      </div>
-      <div className="navBarLink">
-        <NavLink
-          className="inactive"
-          activeClassName="active"
-          exact={true}
-          to="/ingredients"
-        >
-          Ingredients
-        </NavLink>{" "}
-      </div>
-      <div className="navBarLink">
-        <NavLink
-          className="inactive"
-          activeClassName="active"
-          exact={true}
-          to="/random"
-        >
-          Random
-        </NavLink>
-      </div>
-    </div>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link>
+            <NavLink to="/" exact className="navItem">
+              Categories
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink
+              to="/ingredients"
+              className="navItem"
+              activeClassName="active"
+            >
+              Ingredients
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink to="/random" className="navItem">
+              Random
+            </NavLink>
+          </Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="Search"
+            className="mx-2"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <Link to={`/search/${searchTerm}`}>
+            <Button className="searchButton" onClick={() => setSearchTerm("")}>
+              Search
+            </Button>
+          </Link>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
---
+++ src/pages/SearchPage/SearchPage.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import DrinkCard from "../../components/DrinkCard/DrinkCard";
+import "./SearchPage.scss";
+
+export default function SearchPage() {
+  const route_parameters = useParams();
+  const currentSearchTerm = route_parameters.searchTerm;
+
+  const [searchDrinks, setSearchDrinks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${currentSearchTerm}`
+        )
+        .then((response) => {
+          setSearchDrinks(response.data.drinks);
+        });
+    };
+    fetchData();
+  }, [currentSearchTerm]);
+
+  const listOfDrinks = searchDrinks?.map((drink, index) => {
+    return (
+      <DrinkCard key={index} name={drink.strDrink} img={drink.strDrinkThumb} />
+    );
+  });
+
+  return (
+    <div>
+      <p className="mt-3 resultsFound">
+        {listOfDrinks.length} results found for{" "}
+        <span className="searchTerm">{currentSearchTerm}</span>
+      </p>
+      <div className="listOfDrinks row">{listOfDrinks}</div>
+    </div>
+  );
+}
